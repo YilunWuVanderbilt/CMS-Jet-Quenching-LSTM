f9f1e38205093a1e46daf7335410db21458b4108
Yilun Wu <106136747+YilunWuVanderbilt@users.noreply.github.com>
2024-11-12 12:35:58 -0600

    Update root_to_pu14.sh

--- DelphesConverter/root_to_pu14.sh
+++ DelphesConverter/root_to_pu14.sh
@@ -1,12 +1,13 @@
-#basedir=/store/user/yilun/thermal
-basedir=/store/user/yilun/jewel/eventfiles
-#basedir=/store/user/yilun/jewel_vac/eventfiles
+basedir_thermal=/store/user/yilun/thermal
+basedir_jewelmed=/store/user/yilun/jewel/eventfiles
+basedir_jewwelvac=/store/user/yilun/jewel_vac/eventfiles
 
 for i in {1..100}
 do
   echo $i
-  #./root_to_pu14_CMS $basedir/Angantyr_Delphes/Angantyr_Delphes_${i}.root /nobackup/user/wuy55/jetml_data/thermal/Angantyr_pu14/EFlow/angantyr_${i}.pu14
-  ./root_to_pu14_CMS $basedir/root/jewel_R_${i}.root $basedir/pu14_EFlow_1/jewel_R_${i}.pu14
-  #./root_to_pu14_CMS $basedir/Angantyr_Delphes_pp/Angantyr_Delphes_${i}.root $basedir/pu14_EFlow_pp/angantyr_${i}.pu14
+  
+  ./root_to_pu14_EFlow $basedir_thermal/Angantyr_Delphes/Angantyr_Delphes_${i}.root$ basedir_thermal/thermal/Angantyr_pu14/EFlow/angantyr_${i}.pu14
+  ./root_to_pu14_EFlow $basedir_jewelmed/root/jewel_R_${i}.root $basedir_jewelmed/pu14_EFlow_1/jewel_R_${i}.pu14
+  ./root_to_pu14_EFlow $basedir_jewelvac/root/jewel_R_${i}.root $basedir_jewelvac/pu14_EFlow_1/jewel_R_${i}.pu14
   #./root_to_pu14_track $basedir/root/jewel_R_${i}.root $basedir/pu14_Track_1/jewel_R_${i}.pu14
 done
