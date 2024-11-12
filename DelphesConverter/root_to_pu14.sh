4b9ddebc37f903aa8bfda73cc5699e909b270cb7
Yilun Wu <106136747+YilunWuVanderbilt@users.noreply.github.com>
2024-11-12 13:01:01 -0600

    Update root_to_pu14.sh

--- DelphesConverter/root_to_pu14.sh
+++ DelphesConverter/root_to_pu14.sh
@@ -6,7 +6,7 @@ for i in {1..100}
 do
   echo $i
   
-  ./root_to_pu14_EFlow $basedir_thermal/Angantyr_Delphes/Angantyr_Delphes_${i}.root$ basedir_thermal/thermal/Angantyr_pu14/EFlow/angantyr_${i}.pu14
+  ./root_to_pu14_EFlow $basedir_thermal/Angantyr_Delphes/Angantyr_Delphes_${i}.root$ basedir_thermal/Angantyr_pu14//angantyr_${i}.pu14
   ./root_to_pu14_EFlow $basedir_jewelmed/root/jewel_R_${i}.root $basedir_jewelmed/pu14_EFlow_1/jewel_R_${i}.pu14
   ./root_to_pu14_EFlow $basedir_jewelvac/root/jewel_R_${i}.root $basedir_jewelvac/pu14_EFlow_1/jewel_R_${i}.pu14
   #./root_to_pu14_track $basedir/root/jewel_R_${i}.root $basedir/pu14_Track_1/jewel_R_${i}.pu14
